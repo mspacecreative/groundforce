88ac75892ceaf6d30ac0022739a287914ce71416
Matt <matt@mspacecreative.com>
2021-03-21 16:18:46 -0300

    c

--- functions/featuredimage.php
+++ functions/featuredimage.php
@@ -5,7 +5,7 @@ function featuredImage($size) {
 	$defaultimg = get_template_directory_uri() . '/assets/img/placeholders/front-garden.jpg';
 	
 	if ( !empty($featuredimg) ) {
-		echo '<img data-object-fit="cover" src="' . $featuredimg . '" class="object-fit" alt="' . $imgalt . '">';
+		echo '<img data-object-fit="cover" src="' . $featuredimg . '" class="object-fit" alt="' if ($imgalt): $imgalt else . __('Ground Force Property Services') . '">';
 	} else {
 		echo '<img data-object-fit="cover" src="' . $defaultimg . '" class="object-fit" alt="' . __('Ground Force Property Services') . '">';
 	}
