6b2e65c496e71e96f7a9f2d468e07afe74d48db2
Matt <matt@mspacecreative.com>
2021-03-22 12:05:06 -0300

    c

--- single.php
+++ single.php
@@ -1,47 +1,56 @@
 <?php get_header();
 
-if ( have_posts() ) { ?>
+// VARIABLES
+$title = get_the_title();
+$headertitle = get_field('header_title');
+$intro = get_field('intro_copy');
+$content = apply_filters( 'the_content', get_the_content() );
+$hidepageheader = get_field('hide_page_header'); ?>
 
 <div id="main-content">
-	
-	<?php while ( have_posts() ) {
-	the_post();
-	
-	// VARIABLES
-	$title = get_the_title();
-	$intro = get_field('intro_copy');
-	$featuredimg = get_the_post_thumbnail_url( $post->ID, 'large' ); 
-	$imgalt = get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true );
-	$content = apply_filters( 'the_content', get_the_content() ); ?>
-	
+	<div class="innerContainer">
+	 	<h1 class="page_title"><?php echo $title ?></h1>
+	</div>
+		
 	<div class="inner_container">
-		<div class="row no_gutters light_grey_bg middle-lg middle-md">
+	
+	<?php if ( !$hidepageheader ): ?>
+		
+		<div class="row no_gutters light_grey_bg page_header">
 			<div class="col col-lg-6 col-md-6 col-sm-12 col-xs-12">
-				<img data-object-fit="cover" src="<?php echo $featuredimg ?>" class="object-fit" alt="<?php echo $imgalt ?>">
+			<?php featuredImage('large'); ?>
 			</div>
 			
 			<div class="col col-lg-6 col-md-6 col-sm-12 col-xs-12">
-				<div class="col_inner">
+				<div class="col_inner text_align_center">
 					
-					<?php if ( $title && $intro ): ?>
-					<h1 class="post_title"><?php echo $title ?></h1>
+				<?php 
+				if ( is_page('services') ) {
+					queryServiceIcons('services');
+				}
 					
-					<p><?php echo $intro ?></p>
-					<?php endif; ?>
+				if ( $headertitle && $intro ) {
+					echo '<h2>' . $headertitle . '</h2>';
+					echo '<p>' . $intro . '</p>';
+				} ?>
 					
 				</div>
 			</div>
 		</div>
-	</div>
-	
-	<div class="post_content">
-		<?php 
-		echo lightBox();
-		echo $content ?>
+		
+	<?php else:
+	featuredImage('anamorphic');
+	endif; ?>
+		
 	</div>
 
-	<?php } ?>
-
+	<?php 
+	if ( have_posts() ) {
+	 	while ( have_posts() ) { 
+		 	the_post();
+			the_content();
+		}
+	} ?>
 </div>
 
-<?php } get_footer(); ?>
+<?php get_footer(); ?>
