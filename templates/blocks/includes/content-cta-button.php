c42f6104da0060b94b19f0e524df3ceceab86355
Matt <matt@mspacecreative.com>
2021-03-22 10:55:27 -0300

    Don't show CTA unless there is one

--- templates/blocks/includes/content-cta-button.php
+++ templates/blocks/includes/content-cta-button.php
@@ -4,14 +4,16 @@
  	$label = get_sub_field('content_button_label');
  	$internal = get_sub_field('content_internal_link');
  	$external = get_sub_field('content_external_link');
- 	$anchor = get_sub_field('content_anchor_link'); ?>
+ 	$anchor = get_sub_field('content_anchor_link');
  	
+	if ( $internal || $external || $anchor ): ?>
 	<ul class="actions">
 		
 		<li>
 			<a href="<?php if ( $internal ): echo $internal; elseif ( $external ): echo $external; elseif ( $anchor ): echo $anchor; else: echo $internal; endif; ?>" <?php if ( $external ): echo 'target="_blank" '; endif; ?>class="<?php if ( $bgcolor === 'dark' || $bgimgoverlay === 'dark' ): echo 'dark'; elseif ( $bgcolor === 'light' || $bgimgoverlay === 'light' ): echo 'light'; else: echo 'light'; endif; ?> button"><?php if ( $label ): echo $label; else: echo __('Learn more'); endif; ?></a>
 		</li>
 	</ul>
+	<?php endif; ?>
 
 <?php endwhile; ?>
 <?php endif; ?>
