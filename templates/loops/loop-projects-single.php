c64a7194f9820d88fea763fae2b162ef054f0b23
Matt <matt@mspacecreative.com>
2021-03-23 12:12:59 -0300

    c

--- templates/loops/loop-projects-single.php
+++ templates/loops/loop-projects-single.php
@@ -1,58 +1,54 @@
-<section>
+<section class="padding-6em recent-work no-horizontal-overflow">
 	<div class="inner_container ">
 		
-		<?php
+		<?php if ( $heading ): ?>
+		<h2 class="line-rule-right"><span><?php echo esc_html_e('other projects'); ?></span></h2>
+		<?php endif;
+		
 		$args = array(
 			'post_type' => 'project',
 			'posts_per_page' => -1,
+			'post__not_in' => array ($post->ID)
 		);
 			
 		$loop = new WP_Query( $args );
 		if ( $loop->have_posts() ) : ?>
 		
-		<div class="row">
-		
-		<?php while ( $loop->have_posts() ) : $loop->the_post();
-		
-		// VARIABLES
-		$featuredimg = get_the_post_thumbnail_url( get_the_ID(), 'image-tile' );
-		$imgalt = get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true );
-		$title = get_the_title(get_the_ID());
-		$excerpt = get_the_excerpt(get_the_ID()); ?>
-		
-		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 display-flex">
+		<div class="work-carousel margin-top-2em">
 			
-			<div class="row no_gutters bottom_col_spacing">
+			<?php while ( $loop->have_posts() ) : $loop->the_post();
+			
+			// VARIABLES
+			$featuredimg = get_the_post_thumbnail_url( get_the_ID(), 'image-tile' );
+			$imgalt = get_post_meta( get_post_thumbnail_id(), '_wp_attachment_image_alt', true );
+			$title = get_the_title(get_the_ID());
+			$excerpt = get_the_excerpt(get_the_ID()); ?>
+			
+			<div class="col col-lg-3 col-md-3 col-sm-6 col-xs-12">
+				<a href="<?php the_permalink(); ?>">
+					<img src="<?php echo $featuredimg ?>" alt="<?php if ( $imgalt ): echo $imgalt; else: echo bloginfo('name'); endif; ?>">
 					
-				<div class="col col-lg-6 col-md-6 col-sm-6 col-xs-12">
-					<img data-object-fit="cover" src="<?php echo $featuredimg ?>" alt="<?php if ( $imgalt ): echo $imgalt; else: echo bloginfo('name'); endif; ?>" class="object-fit">
-				</div>
+					<?php if ( $title ): ?>
+					<h4><?php echo $title ?></h4>
+					<?php endif; ?>
 					
-				<div class="col col-lg-6 col-md-6 col-sm-6 col-xs-12">
-						
-					<div class="col_inner grey_bg row no_gutters center-lg center-md center-sm center-xs middle-lg middle-md middle-sm middle-xs flex-direction-column-center">
-						<?php if ( $title ): ?>
-						<h2 class="small-h2"><?php echo $title ?></h2>
-						<?php endif; ?>
-								
-						<?php if ( $excerpt ): ?>
-						<p><?php echo $excerpt ?></p>
-						<?php endif; ?>
-								
-						<a href="<?php the_permalink(); ?>" class="button"><?php echo esc_html_e('view project'); ?></a>
-					</div>
-						
-				</div>
+					<?php if ( $excerpt ): ?>
+					<p><?php echo $excerpt ?></p>
+					<?php endif; ?>
 					
+				</a>
 			</div>
-				
-		</div>
-		
-		<?php endwhile; ?>
-		
+			
+			<?php endwhile; ?>
 		</div>
 		
 		<?php endif; wp_reset_query(); ?>
 		
+		<div class="row view-all-work">
+			<div class="col col-lg-12 col-md-12 col-sm-12 col-xs-12 justify-content-end margin-top-2em middle-lg middle-md">
+				<div class="append-arrows"></div>
+				<a href="<?php echo home_url('recent-work'); ?>" class="button align-self-center"><?php echo esc_html_e('view all'); ?></a>
+			</div>
+		</div>
 	</div>
 </section>
